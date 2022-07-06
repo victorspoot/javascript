684412c7c6db86564e9fd70c7bce024d334e86df
victorspoot <v_h_p_s@hotmail.com>
2022-07-06 19:24:54 -0300

    update v1.1

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function ex001() {
-    var nome = document.getElementById('txtnome')
-    var n = nome.value
-    var res = document.querySelector('div#res')
-    res.innerHTML = `Prazer em te conhecer ${n}!`
+function ex002() {
+    var n = document.getElementById('txtnome')
+    var nome = n.value
+    var res = document.querySelector('div#res_dois')
+    res.innerHTML = `Prazer em te conhecer ${nome}!`
 }
+
+function ex003() {
+    var nome = document.getElementById('txtnome')
+    var salario = document.getElementById('sal')
+    var res = document.getElementById('res_tres')
+    var n1 = nome.value
+    var sal = salario.value
+    res.innerHTML = `Nome do funcionario: ${n1}</br>`
+    res.innerHTML += `Salário: R$${sal} reais</br>`
+    res.innerHTML += `O funcionário ${n1} tem um salário de R$${sal} reais.`
+}
+
